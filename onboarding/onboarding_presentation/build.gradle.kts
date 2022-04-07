0472c3831f1127e616ea619d46accbfc540e4199
David Greene <dgdavidgreene@yahoo.com>
2022-04-06 22:02:25 -0700

    Preview

--- onboarding/onboarding_presentation/build.gradle.kts
+++ onboarding/onboarding_presentation/build.gradle.kts
@@ -7,5 +7,4 @@ dependencies {
     // "implementation"(project(Modules.coreUi))
     "implementation"(project(Modules.ui))
     "implementation"(project(Modules.onboardingDomain))
-    "implementation"("androidx.window:window:1.0.0")
 }
