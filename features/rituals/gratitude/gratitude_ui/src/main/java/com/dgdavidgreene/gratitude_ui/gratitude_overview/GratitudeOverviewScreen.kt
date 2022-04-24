92b00590f46f1224c3ffd16e59979502853b72cb
David Greene <dgdavidgreene@yahoo.com>
2022-04-23 18:57:02 -0700

    Overview 1

--- features/rituals/gratitude/gratitude_ui/src/main/java/com/dgdavidgreene/gratitude_ui/gratitude_overview/GratitudeOverviewScreen.kt
+++ features/rituals/gratitude/gratitude_ui/src/main/java/com/dgdavidgreene/gratitude_ui/gratitude_overview/GratitudeOverviewScreen.kt
@@ -1,8 +1,12 @@
 package com.dgdavidgreene.gratitude_ui.gratitude_overview
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -12,7 +16,9 @@ import com.dgdavidgreene.ui.LocalSpacing
 
 @ExperimentalCoilApi
 @Composable
-fun GratitudeOverviewScreen(viewModel: GratitudeOverviewViewModel = hiltViewModel()) {
+fun GratitudeOverviewScreen(
+    viewModel: GratitudeOverviewViewModel = hiltViewModel()
+) {
     val spacing = LocalSpacing.current
     val state = viewModel.state
     val context = LocalContext.current
@@ -20,5 +26,10 @@ fun GratitudeOverviewScreen(viewModel: GratitudeOverviewViewModel = hiltViewMode
         modifier = Modifier
             .fillMaxSize()
             .padding(bottom = spacing.spaceMedium)
-    ) {}
+    ) {
+        item {
+            Text(text = "hi")
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+    }
 }
