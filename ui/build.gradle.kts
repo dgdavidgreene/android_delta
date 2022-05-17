bb0a77675f61e58f59621957118c43368f776aef
David Greene <dgdavidgreene@yahoo.com>
2022-05-16 17:32:30 -0700

    Removed ConstrainLayout 1

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -3,8 +3,6 @@ apply {
 }
 
 dependencies {
-
-    "implementation"("androidx.constraintlayout:constraintlayout-compose:1.0.0")
     "implementation"("androidx.constraintlayout:constraintlayout:2.1.3")
 }
 
